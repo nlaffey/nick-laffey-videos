37e1718481e62c614e85e236fa9dfbcffdb8db2b
Nick Laffey <nick.laffey@gmail.com>
2016-09-17 14:19:42 -0400

    removing unused code/general cleanup

--- js/script.js
+++ js/script.js
@@ -1,57 +1,24 @@
-$(document).ready(function(){
-//	$('.midtown.wrapper').hover(function(){
-//			$('.midtown.vidtitle').html('WATCH');},
-//			function(){
-//			$('.midtown.vidtitle').html('MIDTOWN GREENWAY');
-	//		});
-//
-//	$('.letitdie.wrapper').hover(function(){
-//		$('.letitdie.vidtitle').html('WATCH');},
-//		function(){
-//			$('.letitdie.vidtitle').html('LET IT DIE')();
-//		});
-//	$('.midtown.vidtitle').click(function(){
-//		$('.midtown.vid').show();
-//		$('.midtown.vidtitle').html('LOADING...').delay(2000).fadeOut();
-//		$('.midtown.pic').delay(4000).fadeOut();
-		
-//});
+$(document).ready(function () {
 
+    $('.midtown.link').click(function () {
+        $('.midtown.vidtitle').html('MIDTOWN GREENWAY <BR> <img class="play" src="img/play.gif"> ').fadeIn(2000);
+        $('.midtown.wrapper').show();
+        $('.letitdie.wrapper').hide();
+        $('.nick.wrapper').hide();
+    });
 
-//	$('.letitdie.vidtitle').click(function(){
-//		$('.letitdie.vid').show();
-//		$('.letitdie.vidtitle').html('LOADING...').delay(2000).fadeOut();
-//		$('.letitdie.pic').delay(4000).fadeOut();
-		
-// });
+    $('.letitdie.link').click(function () {
+        $('.letitdie.vidtitle').html('LET IT DIE <BR> <img class="play" src="img/play.gif">').fadeIn(2000);
+        $('.letitdie.wrapper').show();
+        $('.midtown.wrapper').hide();
+        $('.nick.wrapper').hide();
+    });
 
-	$('.midtown.link').click(function(){
-		$('.midtown.vidtitle').html('MIDTOWN GREENWAY <BR> <img class="play" src="img/play.gif"> ').fadeIn(2000);
-		$('.midtown.wrapper').show();
-		$('.letitdie.wrapper').hide();
-		$('.nick.wrapper').hide();
-		
-	
-	});
-
-	$('.letitdie.link').click(function(){
-		$('.letitdie.vidtitle').html('LET IT DIE <BR> <img class="play" src="img/play.gif">').fadeIn(2000);
-		$('.letitdie.wrapper').show();
-		$('.midtown.wrapper').hide();
-		$('.nick.wrapper').hide();
-		
-
-		
-		
-	});
-
-$('.title').click(function(){
-		$('.nick.wrapper').show();
-		$('.midtown.wrapper').hide();
-		$('.letitdie.wrapper').hide();
-		
-	
-	});
+    $('.title').click(function () {
+        $('.nick.wrapper').show();
+        $('.midtown.wrapper').hide();
+        $('.letitdie.wrapper').hide();
+    });
 
 
 });
