d586c03337fb5d2dc964df1a8d85f695efa3e847
Nick Laffey <nick.laffey@gmail.com>
2016-09-18 07:25:36 -0400

    adding type attribute

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ import videoOnClick from './src/js/videoOnClick';
 import fancyBoxCustom from './src/js/fancyBoxCustom';
 import css from '!css!resolve-url!sass?sourceMap!./src/scss/style.scss';
 import $ from './node_modules/jquery/dist/jquery.js';
-$('head').append('<style>' + css + '</style>');
+$('head').append('<style type="text/css">' + css + '</style>');
 videoOnClick();
 fancyBoxCustom();
